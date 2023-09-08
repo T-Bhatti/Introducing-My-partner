882462c61c672fd566c52a7991ccac142653dd5d
T-Bhatti <zahra.tahreem@student.oaklandcc.edu>
2023-09-08 15:03:31 -0400

    Getintro

---
+++ index1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Introducing My Partner</title>
+    </head>
+    <body>
+        <?php
+        echo"</br>Gabe Grosse."
+        . "He is doing Software Engineering he is taking the class because it's required for his major and also he enjoy coding";
+        echo"</br>The list is long so to keep it short and sweet: He had Java, VBA, R, Python, (Data science), HTML5, CSS etc.";
+        echo" </br>He plays video games, some biking, he might start a business for building computers and software/hardware services.";
+        echo"</br>He is not doing anything else except for working at his job.";
+        echo"</br>He don't have anything else to share."
+        ?>
+    </body>
+</html>
